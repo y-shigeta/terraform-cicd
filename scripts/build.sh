671632c5a3c4e0a6687b9d8be6dd8ae93f1a70b6
y-shigeta <shige.7822@gmail.com>
2021-12-21 16:39:44 +0900

    4th update

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -x
 
-if [[ ${CODEBUILD_WEBHOOK_TRIGGER} = 'branch/master' ]]; then
+if [[ ${CODEBUILD_WEBHOOK_TRIGGER} = 'branch/main' ]]; then
   ${CODEBUILD_SRC_DIR}/scripts/apply.sh
 else
   ${CODEBUILD_SRC_DIR}/scripts/plan.sh
