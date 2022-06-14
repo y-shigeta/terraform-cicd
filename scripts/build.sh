0d077b433e0bb14a72cf510550913dfb9688bee6
yasunorishigeta <yasunori.shigeta.z307@dirbato.co.jp>
2022-06-14 10:21:51 +0900

    update

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,6 @@ set -x
 if [[ ${CODEBUILD_WEBHOOK_TRIGGER} = 'branch/main' ]]; then
   ${CODEBUILD_SRC_DIR}/scripts/apply.sh
 else
-#  ${CODEBUILD_SRC_DIR}/scripts/plan.sh
-  ${CODEBUILD_SRC_DIR}/scripts/apply.sh
+  ${CODEBUILD_SRC_DIR}/scripts/plan.sh
+#  ${CODEBUILD_SRC_DIR}/scripts/apply.sh
 fi
