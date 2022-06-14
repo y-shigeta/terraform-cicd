c8710c755d466a6475cdadcf7fd915b47624d0d4
yasunorishigeta <yasunori.shigeta.z307@dirbato.co.jp>
2022-06-14 23:46:39 +0900

    update

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 MESSAGE=$(git log ${CODEBUILD_SOURCE_VERSION} -1 --pretty=format:"%s")
-echo ${MESSAGE}
+echo "Message is ${MESSAGE}"
 CODEBUILD_SOURCE_VERSION=$(echo ${MESSAGE} | cut -f4 -d' ' | sed 's/#/pr\//')
-echo ${CODEBUILD_SOURCE_VERSION}
+echo "CODEBUILD_SOURCE_VERSION is ${CODEBUILD_SOURCE_VERSION}"
 terraform init -input=false -no-color
 terraform apply -input=false -no-color -auto-approve
--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
-terraform -help
+#terraform -help
 
 #BASE_URL=https://github.com/mercari/tfnotify/releases/download
 #DOWNLOAD_URL="${BASE_URL}/v0.3.1/tfnotify_v0.3.1_linux_amd64.tar.gz"
